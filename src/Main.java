a0fd1a1bbf50268f9f9e861cbf7e6a446e34911b
Feedison <zhebrun782@gmail.com>
2022-08-02 00:11:58 +0300

    Added simple check for positive numbers, because sizes of sides can't be negative

--- src/Main.java
+++ src/Main.java
@@ -19,11 +19,13 @@ public class Main {
         /* Checking theorem:
         "The sum of two lengths of the sides of a triangle always greater than the length of the third side."
         */
-        if (a + b > c && a + c > b && b + c > a)
-            System.out.printf("Triangle with sides %d, %d, %d  is possible.", a, b, c);
+        if (a > 0 && b > 0 && c > 0)
+            if (a + b > c && a + c > b && b + c > a)
+                System.out.printf("Triangle with sides %d, %d, %d  is possible.", a, b, c);
+            else
+                System.out.printf("Triangle with sides %d, %d, %d is not possible.", a, b, c);
         else
-            System.out.printf("Triangle with sides %d, %d, %d is not possible.", a, b, c);
-
+            System.out.println("Please, enter positive numbers.");
     }
 
 }
